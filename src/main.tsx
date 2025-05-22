8eea5cd0bbd9c2b3c8802c836d4e4930ef26473d
artham <artham@Sris-MacBook-Pro.local>
2025-05-22 19:59:43 +0530

    Fix: Wrap App component with AuthProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App, { AuthProvider } from './App.tsx'; // Import AuthProvider along with App
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AuthProvider> {/* Wrap the App component with AuthProvider */}
+      <App />
+    </AuthProvider>
+  </React.StrictMode>,
+);
